885175a3b467171d2dba7f145d3af9a4fbacf5b9
lostjarred <lostotaku@outlook.com>
2020-07-14 19:11:50 +1000

    Added itemclass with a return iten on use

---
+++ src/main/java/aa11lo/survivalgearfood/item/ItemFoodReturnItem.java
@@ -0,0 +1,22 @@
+package aa11lo.survivalgearfood.item;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class ItemFoodReturnItem  extends ItemFood{
+    ItemStack ReturnItem; 
+    public ItemFoodReturnItem(int amount, float saturation, Item Returneditem) {
+        super(amount, saturation, false);
+        ReturnItem = new ItemStack(Returneditem); 
+    }
+
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving)
+    {
+        super.onItemUseFinish(stack, worldIn, entityLiving);
+        return this.ReturnItem;
+    }
+    
+}
