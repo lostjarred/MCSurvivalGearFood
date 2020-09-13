a0028d00b49f7035b90f3c003f0ab24e020b28fd
lostjarred <lostotaku@outlook.com>
2020-09-13 14:11:07 +1000

    Add item drink scripts

---
+++ src/main/java/aa11lo/survivalgearfood/item/ItemDrink/ItemDrinkBasic.java
@@ -0,0 +1,12 @@
+package aa11lo.survivalgearfood.item.ItemDrink;
+
+import aa11lo.survivalgearfood.item.ItemBasic;
+
+public class ItemDrinkBasic extends ItemBasic {
+
+    public ItemDrinkBasic(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ src/main/java/aa11lo/survivalgearfood/item/ItemDrink/ItemDrinkReturnItem.java
@@ -0,0 +1,13 @@
+package aa11lo.survivalgearfood.item.ItemDrink;
+
+import net.minecraft.item.Item;
+
+public class ItemDrinkReturnItem extends ItemDrinkBasic {
+
+    public ItemDrinkReturnItem(String name, Item returnitem) {
+        super(name);
+        // TODO Auto-generated constructor stub
+        this.setMaxStackSize(1);
+    }
+    
+}
